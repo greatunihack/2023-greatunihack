2dd786e2557f102f28e0017402d61ce8266ee005
a72994jp <a72994jp@manchester.ac.uk>
2023-05-03 14:50:27 +0100

    Complete landing page

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+import './index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Great Uni Hack 2023</h2>
+        <h3>28 October 2023</h3>
+        <p>Manchester Metropolitan University Business School</p>
+        <a href="https://forms.gle/XFULW1Dk2jo9JdCk6">
+          <button>Register Your Interest</button>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
